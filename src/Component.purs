d05a3bc8a265ae12664a774d3643e8785d7c225b
unmanbearpig <unmanbearpig@gmail.com>
2017-10-09 15:22:27 +0300

    Make it play some sound

--- src/Component.purs
+++ src/Component.purs
@@ -2,17 +2,28 @@ module Component where
 
 import Prelude
 
+import Control.Monad.Aff (Aff)
 import Data.Maybe (Maybe(..))
 
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 
+import Audio.WebAudio.Types (WebAudio, AudioContext, OscillatorNode)
+import Audio.WebAudio.AudioContext as AuCtx
+import Audio.WebAudio.AudioParam as AuParam
+import Audio.WebAudio.Oscillator as AuOsc
+import Audio.WebAudio.GainNode (gain)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Console as Console
+
 data Query a = ToggleState a
 
-type State = { on :: Boolean }
+type State = { on :: Boolean
+             , ctx :: Maybe AudioContext
+             , osc :: Maybe OscillatorNode }
 
-component :: forall m. H.Component HH.HTML Query Unit Void m
+component :: forall eff. H.Component HH.HTML Query Unit Void (Aff ( wau :: WebAudio, console :: CONSOLE | eff ))
 component =
   H.component
     { initialState: const initialState
@@ -23,7 +34,9 @@ component =
   where
 
   initialState :: State
-  initialState = { on: false }
+  initialState = { on: false
+                 , ctx: Nothing
+                 , osc: Nothing }
 
   render :: State -> H.ComponentHTML Query
   render state =
@@ -36,13 +49,30 @@ component =
           [ HE.onClick (HE.input_ ToggleState) ]
           [ HH.text
               if not state.on
-              then "Don't push me"
-              else "I said don't push me!"
+              then "Play"
+              else "Stop"
           ]
       ]
 
-  eval :: Query ~> H.ComponentDSL State Query Void m
+  eval :: Query ~> H.ComponentDSL State Query Void (Aff ( wau :: WebAudio, console :: CONSOLE | eff ))
   eval = case _ of
     ToggleState next -> do
-      H.modify (\state -> { on: not state.on })
+      H.liftEff $ Console.log "starting doing something"
+
+      newCtx <- H.liftEff AuCtx.makeAudioContext
+      newOsc <- H.liftEff $ AuCtx.createOscillator newCtx
+
+      g <- H.liftEff $ AuCtx.createGain newCtx
+      H.liftEff $ AuParam.setValue 1.0 =<< gain g
+
+      H.liftEff $ AuCtx.connect newOsc g
+      H.liftEff $ AuCtx.connect g =<< AuCtx.destination newCtx
+
+      H.liftEff $ Console.log "starting playing"
+      H.liftEff $ AuOsc.startOscillator 0.0 newOsc
+      H.liftEff $ Console.log "started playing"
+
+      H.modify (\state -> { on: not state.on
+                          , ctx: Just newCtx
+                          , osc: Just newOsc })
       pure next
--- src/Main.purs
+++ src/Main.purs
@@ -4,10 +4,12 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
+import Control.Monad.Eff.Console (CONSOLE)
+import Audio.WebAudio.Types (WebAudio)
 
 import Component (component)
 
-main :: Eff (HA.HalogenEffects ()) Unit
+main :: forall eff. Eff (HA.HalogenEffects ( wau :: WebAudio, console :: CONSOLE | eff )) Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
   runUI component unit body
