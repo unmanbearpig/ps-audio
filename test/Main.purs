310e1c7fbaf3ce7c2ffcafefa4409316c127b86a
unmanbearpig <unmanbearpig@gmail.com>
2017-10-21 19:37:27 +0300

    Fix tests

--- test/Main.purs
+++ test/Main.purs
@@ -8,10 +8,10 @@ import Test.Unit (suite, test, timeout)
 import Test.Unit.Main (runTest)
 import Test.Unit.Assert as Assert
 
-import Music
 import Music.MidiNote
 import Music.Pitch
 import Music.LetterNotation
+import Music.Notes
 
 main = runTest do
   suite "music" do
@@ -25,18 +25,18 @@ main = runTest do
     --   Assert.equal (noteIndex B) (noteIndex Si)
 
     test "octaveNote" do
-      Assert.equal (MidiNote 0) (toMidiNote (Note ((-1)) C Natural))
-      Assert.equal (MidiNote 100) (toMidiNote (Note (7) E Natural))
-      Assert.equal (MidiNote 127) (toMidiNote (Note (9) G Natural))
+      Assert.equal (MidiNote 0) (toMidiNote (Note (Octave (-1)) (pitchClass' C Natural)))
+      Assert.equal (MidiNote 100) (toMidiNote (Note (Octave 7) (pitchClass' E Natural)))
+      Assert.equal (MidiNote 127) (toMidiNote (Note (Octave 9) (pitchClass' G Natural)))
 
     test "note pitch" do
-      Assert.equal (Hz 440.0) (toHz (Note (Octave 4) A Natural))
-      Assert.equal (Hz 880.0) (toHz (Note (Octave 5) A Natural))
-      Assert.equal (Hz 220.0) (toHz (Note (Octave 3) A Natural))
-      Assert.equal (Hz 110.0) (toHz (Note (Octave 2) A Natural))
-      Assert.equal (Hz  55.0) (toHz (Note (Octave 1) A Natural))
-      Assert.equal (Hz  27.5) (toHz (Note (Octave 0) A Natural))
-      Assert.equal (Hz  13.75) (toHz (Note (Octave (-1)) A Natural))
+      Assert.equal (Hz 440.0) (toHz (Note (Octave 4) (pitchClass' A Natural)))
+      Assert.equal (Hz 880.0) (toHz (Note (Octave 5) (pitchClass' A Natural)))
+      Assert.equal (Hz 220.0) (toHz (Note (Octave 3) (pitchClass' A Natural)))
+      Assert.equal (Hz 110.0) (toHz (Note (Octave 2) (pitchClass' A Natural)))
+      Assert.equal (Hz  55.0) (toHz (Note (Octave 1) (pitchClass' A Natural)))
+      Assert.equal (Hz  27.5) (toHz (Note (Octave 0) (pitchClass' A Natural)))
+      Assert.equal (Hz  13.75) (toHz (Note (Octave (-1)) (pitchClass' A Natural)))
       -- doesn't match exactly, not sure if correct
       -- Assert.equal (Hz  14.568) (toHz (Note (Octave (-1)) (Note A Sharp)))
 
